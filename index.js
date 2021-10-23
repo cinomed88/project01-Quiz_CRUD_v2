1b6a87474e83f47866600755a0d273f410c1e0a6
Lucas Gong <lucas.w.gong@gmail.com>
2021-10-22 19:35:40 -0700

    Add - register API

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
-const DB_URI = 'mongodb+srv://lucaswgong:hihumi2301@cluster0.3kx5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+const { DB_URI } = require('./config')
+// const DB_URI = 'mongodb+srv://lucaswgong:hihumi2301@cluster0.3kx5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const mongoose = require('mongoose')
+const { User } = require('./models/User')
+
+app.use(bodyParser.urlencoded({entended: true}))
+app.use(bodyParser.json())
+
 mongoose.connect(DB_URI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
@@ -14,6 +20,14 @@ mongoose.connect(DB_URI, {
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userInfo)=> {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({ success: true })
+  })
 })
 
 app.listen(port, () => {
