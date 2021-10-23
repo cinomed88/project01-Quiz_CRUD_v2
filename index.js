37f5608f282e776dfb78a052cd6010c05c35d845
Lucas Gong <lucas.w.gong@gmail.com>
2021-10-22 23:46:34 -0700

    Add - logout router

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/api/users/login', (req, res) => {
 
 })
 
-app.get('/api/users/auth', auth, (req,res) => {
+app.get('/api/users/auth', auth, (req, res) => {
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -69,6 +69,19 @@ app.get('/api/users/auth', auth, (req,res) => {
     role: req.user.role,
     image: req.user.image
   })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id },
+    {token: ""},
+    (err, user) => {
+      if(err) return res.json({success: false, err })
+      return res.status(200).send({
+        success: true
+      })
+    }
+
+  )
 })
 
 app.listen(port, () => {
